20988be18035e2b7b57a2be67cee0ca1f1e86ee7
Gilang Ramadhan <jinxprogilang666@gmail.com>
2025-01-12 16:25:00 +0700

    Use PHLLS instead

--- src/Overview.cpp
+++ src/Overview.cpp
@@ -52,13 +52,13 @@ void CHyprspaceWidget::show() {
     if (oLayerAlpha.empty() && Config::hideRealLayers) {
         for (auto& ls : owner->m_aLayerSurfaceLayers[2]) {
             //ls->startAnimation(false);
-            oLayerAlpha.emplace_back(std::make_tuple(ls, ls->alpha->goal()));
+            oLayerAlpha.emplace_back(std::make_tuple(ls.lock(), ls->alpha->goal()));
             *ls->alpha = 0.f;
             ls->fadingOut = true;
         }
         for (auto& ls : owner->m_aLayerSurfaceLayers[3]) {
             //ls->startAnimation(false);
-            oLayerAlpha.emplace_back(std::make_tuple(ls, ls->alpha->goal()));
+            oLayerAlpha.emplace_back(std::make_tuple(ls.lock(), ls->alpha->goal()));
             *ls->alpha = 0.f;
             ls->fadingOut = true;
         }
