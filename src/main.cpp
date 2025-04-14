8381a34be10ad1c8068b877e0281d6589432a979
zd4y <59004538+zd4y@users.noreply.github.com>
2025-04-13 21:24:42 -0500

    feat: use keysym instead of keycode for exitKey

--- src/main.cpp
+++ src/main.cpp
@@ -255,16 +255,20 @@ void onSwipeEnd(void* thisptr, SCallbackInfo& info, std::any args) {
 // Close overview with configurable key
 void onKeyPress(void* thisptr, SCallbackInfo& info, std::any args) {
     const auto e = std::any_cast<IKeyboard::SKeyEvent>(std::any_cast<std::unordered_map<std::string, std::any>>(args)["event"]);
-    //const auto k = std::any_cast<SKeyboard*>(std::any_cast<std::unordered_map<std::string, std::any>>(args)["keyboard"]);
+    const auto k = std::any_cast<SP<IKeyboard>>(std::any_cast<std::unordered_map<std::string, std::any>>(args)["keyboard"]);
 
-    // Get configured exit key (default to ESC if not configured)
-    const auto exitKey = std::any_cast<Hyprlang::INT>(HyprlandAPI::getConfigValue(pHandle, "plugin:overview:exitKey")->getValue());
-    
-    // If exit key is 0, disable keyboard exit
-    if (exitKey == 0)
+    const auto keycode = e.keycode + 8; // Because to xkbcommon it's +8 from libinput
+    const xkb_keysym_t keysym = xkb_state_key_get_one_sym(k->xkbSymState, keycode);
+
+    // Get configured exit key (default to Escape if not configured)
+    const auto cfgExitKey = std::any_cast<Hyprlang::STRING>(HyprlandAPI::getConfigValue(pHandle, "plugin:overview:exitKey")->getValue());
+    const xkb_keysym_t cfgExitKeysym = xkb_keysym_from_name(cfgExitKey, XKB_KEYSYM_CASE_INSENSITIVE);
+
+    // If exit key is empty, disable keyboard exit
+    if (cfgExitKey[0] == '\0')
         return;
-        
-    if (e.keycode == exitKey) {
+
+    if (keysym == cfgExitKeysym) {
         // close all panels
         bool overviewActive = false;
         for (auto& widget : g_overviewWidgets) {
@@ -501,7 +505,7 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE inHandle) {
     HyprlandAPI::addConfigValue(pHandle, "plugin:overview:disableBlur", Hyprlang::INT{0});
     HyprlandAPI::addConfigValue(pHandle, "plugin:overview:overrideAnimSpeed", Hyprlang::FLOAT{0.0});
     HyprlandAPI::addConfigValue(pHandle, "plugin:overview:dragAlpha", Hyprlang::FLOAT{0.2});
-    HyprlandAPI::addConfigValue(pHandle, "plugin:overview:exitKey", Hyprlang::INT{KEY_ESC});
+    HyprlandAPI::addConfigValue(pHandle, "plugin:overview:exitKey", Hyprlang::STRING{"Escape"});
 
     g_pConfigReloadHook = HyprlandAPI::registerCallbackDynamic(pHandle, "configReloaded", [&] (void* thisptr, SCallbackInfo& info, std::any data) { reloadConfig(); });
     HyprlandAPI::reloadConfig();
